cc679d06d42d152c5d67da285ece39f137c74cd0
DavidBaez-14 <rauldavidbs@ufps.edu.co>
2025-11-07 10:55:46 -0500

    Estructura de persona, base para las demás

---
+++ src/main/java/co/edu/ufps/segundoparcialweb1152383/controllers/PersonaController.java
@@ -0,0 +1,5 @@
+package co.edu.ufps.segundoparcialweb1152383.controllers;
+
+public class PersonaController {
+
+}
---
+++ src/main/java/co/edu/ufps/segundoparcialweb1152383/services/PersonaServices.java
@@ -0,0 +1,4 @@
+package co.edu.ufps.segundoparcialweb1152383.services;
+
+public class PersonaServices {
+}
