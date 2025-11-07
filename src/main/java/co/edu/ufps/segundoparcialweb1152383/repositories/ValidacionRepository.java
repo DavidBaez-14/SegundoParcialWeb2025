9ca752f9b69b9f91ae050fccd043e477106b1d29
DavidBaez-14 <rauldavidbs@ufps.edu.co>
2025-11-07 12:05:34 -0500

    Faltaba el repository de la validacion

---
+++ src/main/java/co/edu/ufps/segundoparcialweb1152383/repositories/ValidacionRepository.java
@@ -0,0 +1,10 @@
+package co.edu.ufps.segundoparcialweb1152383.repositories;
+
+import co.edu.ufps.segundoparcialweb1152383.models.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ValidacionRepository extends JpaRepository<Persona, Integer>
+{}
+
