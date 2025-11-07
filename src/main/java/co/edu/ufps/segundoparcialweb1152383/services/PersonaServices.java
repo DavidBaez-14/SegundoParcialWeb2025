c5333a8612f7448db0dd3b7e53b27dbdf79e2a22
DavidBaez-14 <rauldavidbs@ufps.edu.co>
2025-11-07 11:39:48 -0500

    api persona, no me dejaba subirla

--- src/main/java/co/edu/ufps/segundoparcialweb1152383/services/PersonaServices.java
+++ src/main/java/co/edu/ufps/segundoparcialweb1152383/services/PersonaServices.java
@@ -1,4 +1,30 @@
 package co.edu.ufps.segundoparcialweb1152383.services;
 
+import co.edu.ufps.segundoparcialweb1152383.models.Persona;
+import co.edu.ufps.segundoparcialweb1152383.repositories.PersonaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class PersonaServices {
+
+    @Autowired
+    private PersonaRepository personaRepository;
+
+    // Listar todas las personas
+    public List<Persona> listar() {
+        return personaRepository.findAll();
     }
+
+    public Persona obtenerPorId(Integer id) {
+        return personaRepository.findById(id).orElse(null);
+    }
+
+    // Crear una nueva persona
+    public Persona crear(Persona persona) {
+        return personaRepository.save(persona);
+    }
+
+}
+
